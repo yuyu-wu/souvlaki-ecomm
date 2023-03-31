2e95a2dea103d5aca946cd8e5b21b691103552f4
yuyu <swharriet2012@gmail.com>
2023-03-31 12:18:55 +1100

    app.js cleanup

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require('path');  // comes with Node
 const engine = require('ejs-mate');
 
 app.engine('ejs', engine);
+// Tell app to use ejs
 app.set('view engine', 'ejs');
+// Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }));
+// Serving static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -18,10 +20,6 @@ app.get('/ordernow', (req, res) => {
 	res.render('ordernow')
 });
 
-app.get('/menu', (req, res) => {
-	res.render('menu')
-});
-
 app.get('/ourstory', (req, res) => {
 	res.render('ourstory');
 });
@@ -30,13 +28,6 @@ app.get('/shoppingcart', (req, res) => {
 	res.render('shoppingcart');
 });
 
-// app.use((err, req, res, next) => {
-// 	const { statusCode = 500 } = err;
-// 	if (!err.message) err.message = 'Oh no something went wrong!';
-// 	res.status(statusCode).render('error', { err });
-// });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log(`Listening on port ${port}!`);
+app.listen(3000, () => {
+	console.log("Listening on 3000!");
 });
